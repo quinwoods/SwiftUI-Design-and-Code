6400f2984f4d6c26d0d48987734f91b0b6229e62
Quin'darius <quinwoodz@gmail.com>
2020-02-22 07:45:14 -0500

    Initial Commit

---
+++ SwiftUI Design and Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Design and Code
+//
+//  Created by Quin’darius Lyles-Woods on 2/22/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
