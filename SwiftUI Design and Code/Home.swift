4bcfb6604b1cd0a7608ed3f19cb0532777073903
Quin'darius <quinwoodz@gmail.com>
2020-02-24 18:51:19 -0500

    More Gesture Practice and some 3D effects

--- SwiftUI Design and Code/Home.swift
+++ SwiftUI Design and Code/Home.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct Home: View {
     @State var showProfile = false
+    @State var viewState = CGSize.zero
     var body: some View {
         ZStack {
             Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
@@ -37,13 +38,32 @@ struct Home: View {
             .background(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 20)
-            .rotation3DEffect(Angle(degrees: showProfile ? -10 : 0 ), axis: (x: 10.0, y: 0, z: 0))
+            .rotation3DEffect(Angle(degrees: showProfile ? Double(viewState.height / 5) - 10 : 0 ), axis: (x: 10.0, y: 0, z: 0))
             .offset(y: showProfile ? -450 : 0)
             .scaleEffect(showProfile ? 0.9 : 1)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
             .edgesIgnoringSafeArea(.all)
-            MenuView().offset(y: showProfile ? 0 : 600)
+            MenuView()
+                .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.001))
+                .offset(y: showProfile ? 0 : 700)
+                .offset(y: viewState.height)
                 .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .onTapGesture {
+                        self.showProfile.toggle()
+                }
+        .gesture(
+            DragGesture().onChanged{value in
+                self.viewState = value.translation
+            }
+            .onEnded{value in
+                if self.viewState.height > 30{
+                    self.showProfile = false
+                }
+                self.viewState = .zero
+                
+            }
+            
+            )
         }
     }
 }
