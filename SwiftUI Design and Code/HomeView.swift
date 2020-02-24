05d46125e7a063a3ef0c3fefcbf7b9078b9a2457
Quin'darius <quinwoodz@gmail.com>
2020-02-24 18:58:47 -0500

    HomeView file added

---
+++ SwiftUI Design and Code/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  SwiftUI Design and Code
+//
+//  Created by Quin’darius Lyles-Woods on 2/24/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
