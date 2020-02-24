3d2241c06ebada9e1c199ce85e1445c5a110fadc
Quin'darius <quinwoodz@gmail.com>
2020-02-24 18:04:56 -0500

    Home Screen added

---
+++ SwiftUI Design and Code/Home.swift
@@ -0,0 +1,21 @@
+//
+//  Home.swift
+//  SwiftUI Design and Code
+//
+//  Created by Quin’darius Lyles-Woods on 2/24/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
