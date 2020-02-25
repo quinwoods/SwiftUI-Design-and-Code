11990fd786e2d29a9cee1484a1fb08659b02fe1e
Quin'darius <quinwoodz@gmail.com>
2020-02-25 12:20:31 -0500

    Created UpdateStore

---
+++ SwiftUI Design and Code/UpdateStore.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateStore.swift
+//  SwiftUI Design and Code
+//
+//  Created by Quin’darius Lyles-Woods on 2/25/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateStore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateStore_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateStore()
+    }
+}
