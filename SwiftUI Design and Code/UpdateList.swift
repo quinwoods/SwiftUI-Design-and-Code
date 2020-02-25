0a586030b3d324cc268b1a350cad3b5efae2c741
Quin'darius <quinwoodz@gmail.com>
2020-02-25 12:18:37 -0500

    NavigationView: nav bar buttons, layout of the navigation, pre built animations

--- SwiftUI Design and Code/UpdateList.swift
+++ SwiftUI Design and Code/UpdateList.swift
@@ -10,7 +10,35 @@ import SwiftUI
 
 struct UpdateList: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List(updateData) { update in
+                NavigationLink(destination: Text(update.text)) {
+                    HStack {
+                        Image(update.image)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                            .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                            .cornerRadius(20)
+                            .padding(.trailing, 4)
+                        VStack(alignment: .leading, spacing: 8.0) {
+                            Text(update.title)
+                                .font(.system(size: 20 , weight: .bold))
+                            Text(update.text)
+                                .lineLimit(2)
+                                .font(.subheadline)
+                                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                            Text(update.date)
+                                .font(.caption)
+                                .fontWeight(.bold)
+                                .foregroundColor(.secondary)
+                            
+                        }
+                    }.padding(.vertical, 8)
+                }
+                .navigationBarTitle(Text("Updates"))
+            }
+        }
     }
 }
 
@@ -19,3 +47,18 @@ struct UpdateList_Previews: PreviewProvider {
         UpdateList()
     }
 }
+
+struct Update: Identifiable {
+    var id =  UUID()
+    var image: String
+    var title: String
+    var text: String
+    var date: String
+}
+
+let updateData = [
+    Update(image: "Card1", title: "SwiftUI", text: "text", date: "Jan 1"),
+    Update(image: "Card1", title: "React", text: "text", date: "Jan 1"),
+    Update(image: "Card1", title: "Advanced SwiftUI", text: "text", date: "Jan 1"),
+    Update(image: "Card1", title: "iOS", text: "text", date: "Jan 1"),
+]
