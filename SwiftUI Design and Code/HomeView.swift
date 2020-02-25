f753414f8a48a9e03ce7ea53b3e6a932e41ce883
Quin'darius <quinwoodz@gmail.com>
2020-02-24 19:56:28 -0500

    Allowing for Scrolling within the application. Designing of cards Will be able to use this for any scroll layout

--- SwiftUI Design and Code/HomeView.swift
+++ SwiftUI Design and Code/HomeView.swift
@@ -21,7 +21,17 @@ struct HomeView: View {
                 
                 AvatarView(showProfile: $showProfile)
             }.padding(.horizontal)
+                .padding(.leading, 20)
                 .padding(.top, 30)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack(spacing: 30) {
+                    ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                        SectionVeiw()
+                    }
+                }.padding(30)
+                    .padding(.bottom, 30)
+            }
             Spacer()
         }
     }
@@ -30,5 +40,33 @@ struct HomeView: View {
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView(showProfile: .constant(false))
+    }
+}
+
+struct SectionVeiw: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top) {
+                Text("Prototype desisns in SwiftUI")
+                    .font(.system(size: 24, weight: .bold))
+                    .frame(width: 160, alignment: .leading)
+                    .foregroundColor(Color(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)))
+                Spacer()
+                Image("Logo1")
+                
+                
+            }
+            Text("18 sections".uppercased())
+                .frame(maxWidth: .infinity, alignment: .leading)
+            Image("Card1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 210)
+        }
+        .padding()
+        .frame(width: 275, height: 275)
+        .background(Color("card1"))
+        .cornerRadius(30)
+        .shadow(color: Color("card1").opacity(0.3), radius: 20, x: 0, y: 20)
     }
 }
