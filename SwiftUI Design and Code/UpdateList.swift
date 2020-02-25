2e432316169564e3ca83b73a7b417995264a0c45
Quin'darius <quinwoodz@gmail.com>
2020-02-25 11:33:42 -0500

    Added the UpdateList.swift

---
+++ SwiftUI Design and Code/UpdateList.swift
@@ -0,0 +1,21 @@
+//
+//  UpdateList.swift
+//  SwiftUI Design and Code
+//
+//  Created by Quin’darius Lyles-Woods on 2/25/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct UpdateList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UpdateList_Previews: PreviewProvider {
+    static var previews: some View {
+        UpdateList()
+    }
+}
