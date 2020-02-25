5e420e6961a3d6e9350f048601a3ea56026a3aaa
Quin'darius <quinwoodz@gmail.com>
2020-02-25 13:25:04 -0500

    Combine and editing data from multiple locations

--- SwiftUI Design and Code/UpdateList.swift
+++ SwiftUI Design and Code/UpdateList.swift
@@ -9,38 +9,57 @@
 import SwiftUI
 
 struct UpdateList: View {
+    @ObservedObject var store = UpdateStore()
+    
+    func addUpdate(){
+        store.updates.append(Update(image: "Card1", title: "New Item", text: "Text", date: "Sep 14"))
+    }
+    
     var body: some View {
         NavigationView {
-            List(updateData) { update in
-                NavigationLink(destination: Text(update.text)) {
-                    HStack {
-                        Image(update.image)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 80, height: 80)
-                            .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
-                            .cornerRadius(20)
-                            .padding(.trailing, 4)
-                        VStack(alignment: .leading, spacing: 8.0) {
-                            Text(update.title)
-                                .font(.system(size: 20 , weight: .bold))
-                            Text(update.text)
-                                .lineLimit(2)
-                                .font(.subheadline)
-                                .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
-                            Text(update.date)
-                                .font(.caption)
-                                .fontWeight(.bold)
-                                .foregroundColor(.secondary)
-                            
-                        }
-                    }.padding(.vertical, 8)
+            List{
+                ForEach(store.updates) { update in
+                    NavigationLink(destination: Text(update.text)) {
+                        HStack {
+                            Image(update.image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 80, height: 80)
+                                .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)))
+                                .cornerRadius(20)
+                                .padding(.trailing, 4)
+                            VStack(alignment: .leading, spacing: 8.0) {
+                                Text(update.title)
+                                    .font(.system(size: 20 , weight: .bold))
+                                Text(update.text)
+                                    .lineLimit(2)
+                                    .font(.subheadline)
+                                    .foregroundColor(Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)))
+                                Text(update.date)
+                                    .font(.caption)
+                                    .fontWeight(.bold)
+                                    .foregroundColor(.secondary)
+                                
+                            }
+                        }.padding(.vertical, 8)
+                    }
+                    }.onDelete{
+                        index in self.store.updates.remove(at: index.first!)
+                    }
+                .onMove(perform: {
+                    (source: IndexSet, destination: Int) in self.store.updates.move(fromOffsets: source, toOffset: destination)
+                })
                 }
+                    
                 .navigationBarTitle(Text("Updates"))
+                .navigationBarItems(leading: Button(action: self.addUpdate) {
+                    Text("Add Update")
+                }, trailing: EditButton())
             }
         }
     }
-}
+
+
 
 struct UpdateList_Previews: PreviewProvider {
     static var previews: some View {
