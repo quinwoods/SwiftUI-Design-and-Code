e7cb05bb0fe6c9ae1bedc36b89e97b62fd7531d5
Quin'darius <quinwoodz@gmail.com>
2020-02-26 00:05:00 -0500

    Adding new file

---
+++ SwiftUI Design and Code/CourseList.swift
@@ -0,0 +1,22 @@
+//
+//  CourseList.swift
+//  SwiftUI Design and Code
+//
+//  Created by Quin’darius Lyles-Woods on 2/26/20.
+//  Copyright © 2020 Quin’darius Lyles-Woods. All rights reserved.
+//
+
+import SwiftUI
+
+struct CourseList: View {
+    var body: some View {
+        Text("SwiftUI Advanced")
+        .font
+    }
+}
+
+struct CourseList_Previews: PreviewProvider {
+    static var previews: some View {
+        CourseList()
+    }
+}
